2f2b277a75a47a430148a1e940d2d44fa0c83dec
Vladimir <glazyrin-vladimir@rambler.ru>
2016-04-20 01:01:13 +0300

    add lab4

--- mlita/zadanie3/task1/main.cpp
+++ mlita/zadanie3/task1/main.cpp
@@ -8,7 +8,7 @@
 #include <map>
 #include <fstream>
 #include "Dictionary.h"
-
+#define SIZE 8
 using namespace std;
 
 int _tmain()
@@ -17,15 +17,18 @@ int _tmain()
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
-	ifstream inputFile("input.txt");
+	ifstream inputFile("input2.txt");
 
 	int cities;
 	int ways;
 
 	inputFile >> cities;
 	inputFile >> ways;
-
-	std::vector<std::vector<int> > wayMatrix(cities, vector<int>(ways));
+	int d[50];
+	int v[50];
+	int temp;
+	int minindex, min;
+	vector<vector<int> > a(cities, vector<int>(ways));
 	
 	for (size_t i = 0; i < ways; i++)
 	{
@@ -35,16 +38,79 @@ int _tmain()
 		inputFile >> fromPoint;
 		inputFile >> inPoint;
 		inputFile >> way;
-		wayMatrix[fromPoint-1][inPoint-1] = way;
+		a[fromPoint-1][inPoint-1] = way;
 	}
 
 	for (size_t i = 0; i < cities; i++)
 	{
 		for (size_t j = 0; j < cities; j++)
-			cout << wayMatrix[i][j] << " ";
+			cout << a[i][j] << " ";
 		cout << endl;
 	}
-			
+	cout << endl;
+
+	for (int i = 0;i<cities;i++)
+	 {
+		d[i] = 10000;
+		v[i] = 1;
+	}
+	d[0] = 0;
+
+	do {
+		minindex = 10000;
+		min = 10000;
+		for (int i = 0; i<SIZE;i++) {
+			if ((v[i] == 1) && (d[i]<min)) {
+				min = d[i];
+				minindex = i;
+			}
+		}
+		if (minindex != 10000) {
+			for (int i = 0;i<SIZE;i++) {
+				if (a[minindex][i] > 0) {
+					temp = min + a[minindex][i];
+					if (temp < d[i])
+						d[i] = temp;
+				}
+			}
+			v[minindex] = 0;
+		}
+	} while (minindex < 10000);
+
+
+	/*do {
+		cout << "итерация" << endl;
+		minindex = 10000;
+		min = 10000;
+		for (int i = 0; i<cities;i++)
+		{
+			if ((v[i] == 1) && (d[i]<min)) {
+				min = d[i];
+				minindex = i;
+			}
+			cout << min << " " << minindex << endl;
+		}
+		if (minindex != 10000) {
+			for (int i = 0;i<cities;i++) {
+				if (a[minindex][i] > 0) {
+					temp = a[minindex][i];
+					if (d[i] == 10000)
+					{			
+						d[i] = temp;
+					}
+					else
+						if (temp > d[i])
+							d[i] = temp;
+				}
+			}
+			v[minindex] = 0;
+		}
+	} while (minindex < 10000);*/
+
+	for (size_t i = 1; i < cities; i++)
+		cout << d[i] << " ";
+	
+	vector<int> maxWayToCity(cities, 0);
 
 	return 0;
 }
